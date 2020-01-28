a75861b6c9ef065c0a83a7354debb680e81041fd
ghYoon93 <gh.yoon93@gmail.com>
2020-01-28 23:55:36 +0900

    Add memberDTO & insert Method

--- memberServlet/src/member/action/WriteServlet.java
+++ memberServlet/src/member/action/WriteServlet.java
@@ -7,15 +7,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import member.bean.MemberDAO;
+import member.bean.MemberDTO;
+
 
 public class WriteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
 
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId(request.getParameter("id"));
+		memberDTO.setPwd(request.getParameter("Pwd"));
+		memberDTO.setGender(request.getParameter("Gender"));
+		memberDTO.setEmail1(request.getParameter("email1"));
+		memberDTO.setEmail2(request.getParameter("email2"));
+		memberDTO.setTel1(request.getParameter("tel1"));
+		memberDTO.setTel2(request.getParameter("tel2"));
+		memberDTO.setTel3(request.getParameter("tel3"));
+		memberDTO.setZipcode(request.getParameter("zipcode"));
+		memberDTO.setAddr1(request.getParameter("addr1"));
+		memberDTO.setAddr2(request.getParameter("addr2"));
+		
+		MemberDAO memberDAO = MemberDAO.getInstance();
+		memberDAO.insert(memberDTO);
 	}
 
 }
